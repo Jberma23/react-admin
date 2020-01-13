be0a9c938f00463d12c8d87099f908f265baacfb
github username <jesse.ben.berman@gmail.com>
2020-01-13 09:01:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Tabulator from "tabulator-tables"; //import Tabulator library
+import "tabulator-tables/dist/css/tabulator.min.css"; //import Tabulator stylesheet
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends React.Component {
+  componentDidMount() {
+
+    var table = new Tabulator("#example-table", {
+      height: "311px",
+      movableRows: true,
+      groupBy: "Frequency",
+      groupValues: [["Annual", "Monthly", "On-Demand"]],
+      columns: [
+        { rowHandle: true, formatter: "handle", headerSort: false, frozen: true, width: 30, minWidth: 30 },
+        { title: "Name", field: "name", width: 150 },
+        { title: "Category", field: "category" },
+        { title: "Steps", field: "steps" },
+        { title: "Info", field: "info", align: "center", width: 120 },
+        // { title: "Favourite Color", field: "col" },
+        // { title: "Date Of Birth", field: "dob", align: "center", sorter: "date" },
+        // { title: "Driver", field: "car", align: "center", formatter: "tickCross" },
+      ],
+      data: data,
+    });
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <h1>Test</h1>
+        <div id="example-table"></div>
+
+      </div>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,47 @@
+let data = [
+    {
+        frequency: "Monthly", name: "Consumption (OECD) MODs EIA US", category: "Petroleum", steps: [
+            { "step-1-name": "Load GDP PPP Data-File(0m 12s)", "step-1-update": "loaded: 01/07/2020 09:18:32 AM" },
+            { "step-2-name": "Process Dataset (A)(0m 24s)", "step-2-update": "processed: 01/07/2020 10:36:05 AM" },
+            { "step-3-name": "Push Datasets onto 6A(0m 24s)", "step-3-update": "" }]
+    },
+    {
+        frequency: "Monthly", name: "", category: "", steps: [
+            { "step-1": "" },
+            { "step-2": "" },
+            { "step-3": "" }]
+    },
+    {
+        frequency: "Monthly", name: "", category: "", steps: [
+            { "step-1": "" },
+            { "step-2": "" },
+            { "step-3": "" }]
+    },
+    {
+        frequency: "Monthly", name: "", category: "", steps: [
+            { "step-1": "" },
+            { "step-2": "" },
+            { "step-3": "" }]
+    },
+    {
+        frequency: "Monthly", name: "", category: "", steps: [
+            { "step-1": "" },
+            { "step-2": "" },
+            { "step-3": "" }]
+    },
+    {
+        frequency: "Monthly", name: "", category: "", steps: [
+            { "step-1": "" },
+            { "step-2": "" },
+            { "step-3": "" }]
+    },
+    { frequency: "", name: "", category: "", steps: [{ "step-1": "" }, { "step-2": "" }, { "step-3": "" }] },
+    { frequency: "", name: "", category: "", steps: [{ "step-1": "" }, { "step-2": "" }, { "step-3": "" }] },
+    { frequency: "", name: "", category: "", steps: [{ "step-1": "" }, { "step-2": "" }, { "step-3": "" }] },
+    { frequency: "", name: "", category: "", steps: [{ "step-1": "" }, { "step-2": "" }, { "step-3": "" }] },
+    { frequency: "", name: "", category: "", steps: [{ "step-1": "" }, { "step-2": "" }, { "step-3": "" }] },
+    { frequency: "", name: "", category: "", steps: [{ "step-1": "" }, { "step-2": "" }, { "step-3": "" }] },
+
+]
+
+export default data
